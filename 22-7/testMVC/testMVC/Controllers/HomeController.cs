3f0a43c2ea299e135880a86c1c096a530a9041cc
NourShloul <shloulnour31@gmail.com>
2024-07-22 18:14:05 +0300

    firstMVC

---
+++ 22-7/testMVC/testMVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            ViewBag.Message = "this is a Register page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "this is a login page.";
+
+            return View();
+        }
+        public ActionResult Services()
+        {
+            ViewBag.Message = "this is a user services page.";
+
+            return View();
+        }
+
+
+
+    }
+}
